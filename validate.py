79cce4d8317293c548eabe5f8f66c7b76622ede7
Mux-Mastermann <43514294+Mux-Mastermann@users.noreply.github.com>
2020-04-27 21:24:21 +0200

    Adding max input lines and updated file reading

--- validate.py
+++ validate.py
@@ -6,10 +6,8 @@ from tkinter import filedialog
 from zeep import Client
 
 
-print("Setting up WSDL Client")
-# setting up the wsdl
-client = Client(wsdl="http://ec.europa.eu/taxation_customs/vies/"
-                "checkVatService.wsdl")
+# defining maximum number of lines in the input file
+MAX_INPUT_LINES = 100
 
 # setup tkinter
 root = tk.Tk()
@@ -18,6 +16,11 @@ root.withdraw()
 input_path = filedialog.askopenfilename(title="Select File for VAT-ID check",
                                         filetypes=(("text files", "txt"),))
 
+# setting up the wsdl
+print("Setting up WSDL Client")
+client = Client(wsdl="http://ec.europa.eu/taxation_customs/vies/"
+                "checkVatService.wsdl")
+
 # create output path in the same folder as input_path
 output_path = f"{os.path.split(input_path)[0]}/vatcheck_output.txt"
 
@@ -25,19 +28,25 @@ output_path = f"{os.path.split(input_path)[0]}/vatcheck_output.txt"
 with open(output_path, "w") as write_file:
     # opening the check file (has to be one ID per line)
     with open(input_path, "r") as read_file:
+        # checking amount of lines in the file and preserve to much lines
+        lines = read_file.readlines()
+        if len(lines) > MAX_INPUT_LINES:
+            print(f"Input file is too long. Maximum lines: {MAX_INPUT_LINES}")
+            quit()
+
         print("Checking the VAT-IDs...")
-        for line in read_file:
-            # split id after two characters to seperate country and vatnumber
+        for line in lines:
+            # split id after two characters to seperate country and vatno
             id = re.split(r"(\S\S)", line.strip(), 1)
             try:
-                # check the vat with wsdl
-                r = client.service.checkVat(id[1], id[2])
+                # check the vat with wsdl (THE ACTUAL CHECK)
+                response = client.service.checkVat(id[1], id[2])
             except Exception as e:
                 # if error print the error
                 write_file.write(f"{line.strip()}\t{e}\n")
             else:
                 # else print the returned valid-status
-                write_file.write(f"{line.strip()}\t{r['valid']}\n")
+                write_file.write(f"{line.strip()}\t{response['valid']}\n")
 
 # open the results file
 print("Check done! Opening results...")
